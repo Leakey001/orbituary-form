a80dc0432e1d84ba79d1b154bf9fcfef54aed46a
Leakey001 <149203298+Leakey001@users.noreply.github.com>
2024-07-19 22:41:52 +0300

    Update submit_orbituary.js

--- submit_orbituary.js
+++ submit_orbituary.js
@@ -42,35 +42,3 @@ app.post('/submit_obituary', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-view orbituaries
-const express = require('express');
-const mysql = require('libmysql - mysqlnd 8.1.25'); 
-
-const app = express();
-const port = 3036; 
-
-
-const db = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  database: 'obituary_platform'
-});
-
-app.get('/view_obituaries', (req, res) => {
-  db.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting database connection:', err);
-      return res.status(500).send('Database error');
-    }
-
-    const query = 'SELECT * FROM obituaries';
-
-    connection.query(query, (err, results) => {
-      connection.release();
-
-      if (err) {
-        console.error('Error retrieving obituaries:', err);
-        return res.status(500).send('Error retrieving obituaries');
-      }
-
-      res.render('obituaries', { obituaries: results });
